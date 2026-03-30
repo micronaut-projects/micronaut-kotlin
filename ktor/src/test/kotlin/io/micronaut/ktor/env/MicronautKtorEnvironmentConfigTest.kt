22d00724a98b9fc7b8cd51bf590be5996600f6d0
issam liqali <liqaliissam@gmail.com>
2026-03-30 16:08:31 +0100

    expand tests for new ApplicationConfigValue

--- ktor/src/test/kotlin/io/micronaut/ktor/env/MicronautKtorEnvironmentConfigTest.kt
+++ ktor/src/test/kotlin/io/micronaut/ktor/env/MicronautKtorEnvironmentConfigTest.kt
@@ -1,5 +1,8 @@
 package io.micronaut.ktor.env
 
+import io.ktor.server.config.ApplicationConfigValue
+import io.ktor.util.reflect.typeInfo
+import io.micronaut.context.annotation.Property
 import io.micronaut.context.env.Environment
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
@@ -7,6 +10,9 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.function.Consumer
 
+@Property(name = "ktor.test.scalar", value = "value")
+@Property(name = "ktor.test.list[0]", value = "a")
+@Property(name = "ktor.test.list[1]", value = "b")
 @MicronautTest
 class MicronautKtorEnvironmentConfigTest {
     @Inject
@@ -23,5 +29,33 @@ class MicronautKtorEnvironmentConfigTest {
                 config.property(s)
             }, String.format(msg, s))
         })
+    }
+
+    @Test
+    fun testScalarAndListAccess() {
+        val config = MicronautKtorEnvironmentConfig(env = env)
+        val nestedConfig = config.config("ktor.test")
+
+        assertEquals("value", nestedConfig.property("scalar").getString())
+        assertEquals(listOf("a", "b"), nestedConfig.property("list").getList())
+        assertNotNull(nestedConfig.propertyOrNull("scalar"))
+    }
+
+    @Test
+    fun testApplicationConfigValueTypesAndAccessors() {
+        val config = MicronautKtorEnvironmentConfig(env = env)
+        val nestedConfig = config.config("ktor.test")
+
+        val objectValue = config.property("ktor.test")
+        val scalarValue = nestedConfig.property("scalar")
+        val listValue = nestedConfig.property("list")
+
+        assertEquals(ApplicationConfigValue.Type.OBJECT, objectValue.type)
+        assertEquals(ApplicationConfigValue.Type.OBJECT, scalarValue.type)
+        assertEquals(ApplicationConfigValue.Type.OBJECT, listValue.type)
+
+        assertNotNull(objectValue.getMap())
+        assertEquals("value", scalarValue.getString())
+        assertEquals(listOf("a", "b"), listValue.getList())
     }
 }
